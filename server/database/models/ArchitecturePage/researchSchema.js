52b37a49a8ca438e2fba64bc1dea1af1451bd3af
Bhavesh Damor <bhaveshdamor5555@gmail.com>
2022-09-12 09:56:23 +0530

    reaserch schema change

--- server/database/models/ArchitecturePage/researchSchema.js
+++ server/database/models/ArchitecturePage/researchSchema.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
 const Research = new mongoose.Schema({
-    sliderImages:[],
-    title:String,
-    content:String,
-    links:String,
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    }
+  sliderImages: [],
+  title: String,
+  content: String,
+  hasPDF: Boolean,
+  pdfFileURL: "",
+  link: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Research", Research);
